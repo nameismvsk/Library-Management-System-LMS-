55a00583a33b77134a06b11d3d3c29eb9a14c572
nameismvsk <mvsravankumar1@gmail.com>
2025-04-08 22:57:23 +0530

    learning git

--- LibraryManagement.java
+++ LibraryManagement.java
@@ -2,7 +2,7 @@ package LBS;
 import java.util.Scanner;
 
 public class LibraryManagement {
-    public static void main(String args[]) {
+    public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
         
        
